54029aebec6c57b51f36d5e62e983a4e4f2c5b6f
Rajeev <rajeevcbhatia@gmail.com>
2019-05-20 02:00:09 +0200

    removed unused code

--- Nearby/Map/MapViewController.swift
+++ Nearby/Map/MapViewController.swift
@@ -105,15 +105,3 @@ extension MapViewController: MKMapViewDelegate {
     }
     
 }
-
-typealias Edges = (ne: CLLocationCoordinate2D, sw: CLLocationCoordinate2D)
-
-private extension MKMapView {
-    func edgePoints() -> Edges {
-        let nePoint = CGPoint(x: self.bounds.maxX, y: self.bounds.origin.y)
-        let swPoint = CGPoint(x: self.bounds.minX, y: self.bounds.maxY)
-        let neCoord = self.convert(nePoint, toCoordinateFrom: self)
-        let swCoord = self.convert(swPoint, toCoordinateFrom: self)
-        return (ne: neCoord, sw: swCoord)
-    }
-}
